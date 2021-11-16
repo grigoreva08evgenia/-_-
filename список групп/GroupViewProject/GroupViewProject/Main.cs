36a70ee0819a6ca3a486b7a13a8fefb84e5011a7
grigoreva08evgenia <72446984+grigoreva08evgenia@users.noreply.github.com>
2021-11-16 10:43:12 +0900

    Add files via upload

---
+++ список групп/GroupViewProject/GroupViewProject/Main.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace GroupViewProject
+{
+    class Main
+    {
+        // Строка подключения к БД
+        string strConnection = @"Data Source=403-10\LOCALDB#C24B9DA4;Initial Catalog=groupall;Integrated Security=True";
+
+        /// <summary>
+        /// Добавления данных о группе 
+        /// </summary>
+        /// <param name="group"> Класс Group</param>
+        public void AddGroup(Group group)
+        {
+            // 
+            string SqlCmd = $"INSERT INTO [dbo].[group] ([NameGroup],[NumberGroup],[CuratorGroup]) " +
+                $"VALUES ('{group.NameGroup}','{group.NumberGroup}','{group.CuratorGroup}')";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Чтения данных из таблицы group
+        /// </summary>
+        /// <returns></returns>
+        public List<Group> ReadGroup()
+        {
+            string SqlCmd = "SELECT * FROM [dbo].[group]";
+            List<Group> groups = new List<Group>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(strConnection))
+                {
+
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(SqlCmd, connection);
+
+                    // Получаем строки из таблицы
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        // Построчно считываем данные
+                        while (sqlDataReader.Read())
+                        {
+                            groups.Add(new Group()
+                            {
+                                NameGroup = sqlDataReader.GetString(1),
+                                NumberGroup = sqlDataReader.GetString(2),
+                                CuratorGroup = sqlDataReader.GetString(3)
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            return groups;
+        }
+
+    }
+
+
+        /// <summary>
+        ///  Описание таблицы из БД
+        /// </summary>
+        class Group
+        {
+            public int idGroup { get; set; }
+
+            public string NameGroup { get; set; }
+
+            public string NumberGroup { get; set; }
+
+            public string CuratorGroup { get; set; }
+        }
+
+}
---
+++ список групп/GroupViewProject/GroupViewProject/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupViewProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdListView();
+        }
+
+        void UpdListView()
+        {
+            Main main = new Main();
+
+            lvViewGroup.ItemsSource = main.ReadGroup();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Group group = new Group()
+            {
+                NameGroup = tbNameGroup.Text,
+                NumberGroup = tbNumberGroup.Text,
+                CuratorGroup = tbCurator.Text
+            };
+
+            Main main = new Main();
+
+            main.AddGroup(group);
+            UpdListView();
+        }
+
+    }
+}
